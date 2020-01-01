df1f991614fa61ac66659c237511c8daa6e9507d
Jihoon Kim (Hanul) <kimwlgns4270@naver.com>
2020-01-02 00:46:24 +0900

    Change Infomation

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,11 +63,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    new GitHubCalendar("#github-graph", "jihoonkim-tech");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "jihoonkim-tech", selector: "#ghfeed" });
 
 
 });
